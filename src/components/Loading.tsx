046ad405c45703a3a96af2107e9f7680e093cfe9
Arnold Rodriguez <arnolddavidrd@gmail.com>
2025-08-02 00:09:44 -0500

    feat: replace loading text with animated SVG in Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,23 @@
 export default function Loading() {
   return (
     <div className="fixed top-0 left-0 w-full h-screen opacity-95 flex items-center justify-center bg-gray-900 z-20">
-      <p className="text-white text-3xl">Loading...</p>
+      <p className="text-white text-3xl">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="icon icon-tabler icons-tabler-outline icon-tabler-loader-2 animate-spin"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M12 3a9 9 0 1 0 9 9" />
+        </svg>
+      </p>
     </div>
   );
 }
